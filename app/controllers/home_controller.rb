6142819f01cda049e4fc209c681dfc1ba2d9bf2e
nnovak13 <nnovak@fordham.edu>
2014-09-25 18:00:24 -0400

    updated asset html video references

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,6 @@ class HomeController <ApplicationController
 
   private
     def videos
-      ["https://github.com/nnovak13/portfolio_site/blob/master/portfolio_site/app/assets/images/bkg_woods.mp4?raw=true", "https://github.com/nnovak13/portfolio_site/blob/master/portfolio_site/app/assets/images/bkg_ocean.mp4?raw=true",]
+      ["https://github.com/nnovak13/portfolio/blob/master/app/assets/images/bkg_woods.mp4?raw=true", "https://github.com/nnovak13/portfolio/blob/master/app/assets/images/bkg_ocean.mp4?raw=true","https://github.com/nnovak13/portfolio/blob/master/app/assets/images/bkg_actual_ocean.mp4?raw=true",]
     end
 end
